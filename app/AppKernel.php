0eae6f48b5ad7017cb1b70e71a4379baf7cb9289
Javier Eguíluz <javier.eguiluz@gmail.com>
2011-05-05 13:17:41 +0200

    Actualizados los archivos de configuración y el kernel de la aplicación para tener en cuenta los cambios introducidos por el nuevo bundle DesymfonyBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Acme\DemoBundle\AcmeDemoBundle(),
             
+            new Desymfony\DesymfonyBundle\DesymfonyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
