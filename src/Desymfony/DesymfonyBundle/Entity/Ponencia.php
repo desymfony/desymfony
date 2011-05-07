2d9c5d102d290f0ccc4463af34546fc44559ace2
Nacho Martín <nitram.ohcan@gmail.com>
2011-05-07 19:08:41 +0200

    [fixed #2] [fixed #3] [fixed #4] corro generate entities y arreglo relación many-to-many en annotations

--- src/Desymfony/DesymfonyBundle/Entity/Ponencia.php
+++ src/Desymfony/DesymfonyBundle/Entity/Ponencia.php
@@ -55,11 +55,11 @@ class Ponencia
     protected $ponente;
 
     /**
-     * @orm:ManyToMany(targetEntity="Usuario", mappedBy="ponencias")
-     * @orm:JoinTable(name="ponencias_usuarios",
+     * @orm:ManyToMany(targetEntity="Usuario", inversedBy="ponencias")
+     * @orm:JoinTable(name="ponencia_usuario",
      *      joinColumns={@orm:JoinColumn(name="usuario_id", referencedColumnName="id")},
      *      inverseJoinColumns={@orm:JoinColumn(name="ponencia_id", referencedColumnName="id")}
-     *      )
+     * )
      */
     protected $usuarios;
 
@@ -67,4 +67,174 @@ class Ponencia
         $this->usuarios = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string $titulo
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string $slug
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set descriptcion
+     *
+     * @param text $descriptcion
+     */
+    public function setDescriptcion($descriptcion)
+    {
+        $this->descriptcion = $descriptcion;
+    }
+
+    /**
+     * Get descriptcion
+     *
+     * @return text $descriptcion
+     */
+    public function getDescriptcion()
+    {
+        return $this->descriptcion;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param datetime $fecha
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return datetime $fecha
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set duracion
+     *
+     * @param integer $duracion
+     */
+    public function setDuracion($duracion)
+    {
+        $this->duracion = $duracion;
+    }
+
+    /**
+     * Get duracion
+     *
+     * @return integer $duracion
+     */
+    public function getDuracion()
+    {
+        return $this->duracion;
+    }
+
+    /**
+     * Set idioma
+     *
+     * @param string $idioma
+     */
+    public function setIdioma($idioma)
+    {
+        $this->idioma = $idioma;
+    }
+
+    /**
+     * Get idioma
+     *
+     * @return string $idioma
+     */
+    public function getIdioma()
+    {
+        return $this->idioma;
+    }
+
+    /**
+     * Set ponente
+     *
+     * @param Desymfony\DesymfonyBundle\Entity\Ponente $ponente
+     */
+    public function setPonente(\Desymfony\DesymfonyBundle\Entity\Ponente $ponente)
+    {
+        $this->ponente = $ponente;
+    }
+
+    /**
+     * Get ponente
+     *
+     * @return Desymfony\DesymfonyBundle\Entity\Ponente $ponente
+     */
+    public function getPonente()
+    {
+        return $this->ponente;
+    }
+
+    /**
+     * Add usuarios
+     *
+     * @param Desymfony\DesymfonyBundle\Entity\Usuario $usuarios
+     */
+    public function addUsuarios(\Desymfony\DesymfonyBundle\Entity\Usuario $usuarios)
+    {
+        $this->usuarios[] = $usuarios;
+    }
+
+    /**
+     * Get usuarios
+     *
+     * @return Doctrine\Common\Collections\Collection $usuarios
+     */
+    public function getUsuarios()
+    {
+        return $this->usuarios;
+    }
 }
--- src/Desymfony/DesymfonyBundle/Entity/Ponente.php
+++ src/Desymfony/DesymfonyBundle/Entity/Ponente.php
@@ -67,4 +67,194 @@ class Ponente
       $this->ponencias = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string $nombre
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellidos
+     *
+     * @param string $apellidos
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+    }
+
+    /**
+     * Get apellidos
+     *
+     * @return string $apellidos
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set biografia
+     *
+     * @param text $biografia
+     */
+    public function setBiografia($biografia)
+    {
+        $this->biografia = $biografia;
+    }
+
+    /**
+     * Get biografia
+     *
+     * @return text $biografia
+     */
+    public function getBiografia()
+    {
+        return $this->biografia;
+    }
+
+    /**
+     * Set telefono
+     *
+     * @param string $telefono
+     */
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+    }
+
+    /**
+     * Get telefono
+     *
+     * @return string $telefono
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string $url
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string $email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set twitter
+     *
+     * @param string $twitter
+     */
+    public function setTwitter($twitter)
+    {
+        $this->twitter = $twitter;
+    }
+
+    /**
+     * Get twitter
+     *
+     * @return string $twitter
+     */
+    public function getTwitter()
+    {
+        return $this->twitter;
+    }
+
+    /**
+     * Set linkedin
+     *
+     * @param string $linkedin
+     */
+    public function setLinkedin($linkedin)
+    {
+        $this->linkedin = $linkedin;
+    }
+
+    /**
+     * Get linkedin
+     *
+     * @return string $linkedin
+     */
+    public function getLinkedin()
+    {
+        return $this->linkedin;
+    }
+
+    /**
+     * Add ponencias
+     *
+     * @param Desymfony\DesymfonyBundle\Entity\Ponencia $ponencias
+     */
+    public function addPonencias(\Desymfony\DesymfonyBundle\Entity\Ponencia $ponencias)
+    {
+        $this->ponencias[] = $ponencias;
+    }
+
+    /**
+     * Get ponencias
+     *
+     * @return Doctrine\Common\Collections\Collection $ponencias
+     */
+    public function getPonencias()
+    {
+        return $this->ponencias;
+    }
 }
