8815f9cdf5b021d9380174719ea668377ef7ac9f
Nacho Martín <nitram.ohcan@gmail.com>
2011-05-07 17:09:32 +0200

    [issue #4] relleno entidad Usuario

--- src/Desymfony/DesymfonyBundle/Entity/Usuario.php
+++ src/Desymfony/DesymfonyBundle/Entity/Usuario.php
@@ -11,5 +11,50 @@ namespace Desymfony\DesymfonyBundle\Entity;
  */
 class Usuario
 {
-    
+    /**
+     * @orm:Id
+     * @orm:Column(type="integer")
+     * @orm:GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id; 
+
+    /**
+    * @orm:Column(type="string")
+    */
+    protected $nombre;
+
+    /**
+    * @orm:Column(type="string")
+    */
+    protected $apellidos;
+
+    /**
+    * @orm:Column(type="string")
+    */
+    protected $dni;
+
+    /**
+    * @orm:Column(type="string")
+    */
+    protected $direccion;
+
+    /**
+    * @orm:Column(type="string")
+    */
+    protected $telefono;
+
+    /**
+    * @orm:Column(type="string")
+    */
+    protected $email;
+
+    /**
+    * @orm:Column(type="string")
+    */
+    protected $password;
+
+    /**
+    * @ManyToMany(targetEntity="Ponencia", mappedBy="usuarios")
+    */
+    protected $ponencias;
 }
