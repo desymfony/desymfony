8e0251ad81fc4329f4d4aa80c17927403b4cd8b2
Nacho Martín <nitram.ohcan@gmail.com>
2011-05-07 16:02:56 +0200

    [issue #2] Añado entidad ponencia

--- src/Desymfony/DesymfonyBundle/Entity/Ponencia.php
+++ src/Desymfony/DesymfonyBundle/Entity/Ponencia.php
@@ -11,5 +11,56 @@ namespace Desymfony\DesymfonyBundle\Entity;
  */
 class Ponencia
 {
-    
+    /**
+     * @orm:Id
+     * @orm:Column(type="integer")
+     * @orm:GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @orm:Column(type="string")
+     */
+    protected $titulo;
+
+    /**
+     * @orm:Column(type="string")
+     */
+    protected $slug;
+
+    /**
+     * @orm:Column(type="text")
+     */
+    protected $descriptcion;
+
+    /**
+     * @orm:Column(type="datetime")
+     */
+    protected $fecha;
+
+    /**
+     * @orm:Column(type="integer")
+     */
+    protected $duracion;
+
+    /**
+     * @orm:Column(type="string", length="2")
+     */
+    protected $idioma;
+
+    /**
+     * @orm:ManyToOne(targetEntity="Ponente", inversedBy="ponencias", cascade={"remove"})
+     * @orm:JoinColumn(name="ponente_id", referencedColumnName="id")
+     */
+    protected $ponente;
+
+    /**
+     * @ManyToMany(targetEntity="Usuario", mappedBy="ponencias")
+     * @JoinTable(name="ponencias_usuarios",
+     *      joinColumns={@JoinColumn(name="usuario_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@JoinColumn(name="ponencia_id", referencedColumnName="id")}
+     *      )
+     */
+    protected $usuarios;
+
 }
