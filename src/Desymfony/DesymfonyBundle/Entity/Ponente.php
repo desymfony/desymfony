05725a1c601f6f3b327ba1b0616a389b32e7a55b
Nacho Martín <nitram.ohcan@gmail.com>
2011-05-07 16:57:42 +0200

    [issue #3] relleno entidad Ponente

--- src/Desymfony/DesymfonyBundle/Entity/Ponente.php
+++ src/Desymfony/DesymfonyBundle/Entity/Ponente.php
@@ -11,5 +11,56 @@ namespace Desymfony\DesymfonyBundle\Entity;
  */
 class Ponente
 {
-    
+    /**
+    * @orm:Id
+    * @orm:Column(type="integer")
+    * @orm:GeneratedValue(strategy="IDENTITY")
+    */
+    protected $id;
+
+    /**
+     * @orm:Column(type="string")
+     */
+    protected $nombre;
+
+    /**
+     * @orm:Column(type="string")
+     */
+    protected $apellidos;
+
+    /**
+     * @orm:Column(type="text")
+     */
+    protected $biografia;
+
+    /**
+     * @orm:Column(type="string")
+     */
+    protected $telefono;
+
+    /**
+     * @orm:Column(type="string")
+     */
+    protected $url;
+
+    /**
+     * @orm:Column(type="string")
+     */
+    protected $email;
+
+    /**
+     * @orm:Column(type="string")
+     */
+    protected $twitter;
+
+    /**
+     * @orm:Column(type="string")
+     */
+    protected $linkedin;
+
+    /**
+     * @OneToMany(targetEntity="Ponencia", mappedBy="ponente")
+     */
+    private $ponencias;
+
 }
