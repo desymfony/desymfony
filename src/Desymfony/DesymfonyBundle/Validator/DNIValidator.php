9e54143109bfc70367601ba2c0716312d3efefbc
Javier López <javier.lopez@flai.es>
2011-05-17 17:13:19 +0200

    [Issue #35] Modificado el validador para cumplir la regla de la letra

--- src/Desymfony/DesymfonyBundle/Validator/DNIValidator.php
+++ src/Desymfony/DesymfonyBundle/Validator/DNIValidator.php
@@ -10,6 +10,14 @@ class DNIValidator extends ConstraintValidator
     public function isValid($value, Constraint $constraint)
     {
         $this->setMessage($constraint->message);
-        return preg_match("/^\d{1,8}[a-zA-Z]{1}$/",$value);
+        if(preg_match("/^(\d{1,8})([a-zA-Z]{1})$/",$value, $matches)){
+            return $this->letra_nif($matches[1]) == $matches[2];
+        }else{
+            return false;
+        }
+    }
+
+    protected function letra_nif($dni) {
+        return substr("TRWAGMYFPDXBNJZSQVHLCKE",strtr($dni,"XYZ","012")%23,1);
     }
 }
