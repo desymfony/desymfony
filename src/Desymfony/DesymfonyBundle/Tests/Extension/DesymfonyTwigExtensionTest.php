5019a5d5cc5bb5001b1b52036d6da4714ddaa636
Javier López <javier.lopez@flai.es>
2011-05-29 18:06:26 +0200

    Issue #64 Creado el filtro auto_link_text e Issue #19 creadas pruebas unitarias para probar el filtro

---
+++ src/Desymfony/DesymfonyBundle/Tests/Extension/DesymfonyTwigExtensionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Desymfony\DesymfonyBundle\Extension;
+
+class DesymfonyTwigExtensionTest extends \PHPUnit_Framework_TestCase{
+
+    public function testURLesConvertidaAEnlace()
+    {
+        $texto          = 'www.google.es';
+        $texto_esperado = '<a href="http://www.google.es" target="_blank" >www.google.es</a>';
+        $texto_filtrado = DesymfonyTwigExtension::auto_link_text($texto);
+
+        $this->assertEquals( $texto_esperado, $texto_filtrado);
+    }
+
+    public function testAnchorsNoPasanPorElFiltro()
+    {
+        $texto          = '<a href="http://www.google.es" target="_blank" >www.yahoo.es</a>';
+        $texto_esperado = '<a href="http://www.google.es" target="_blank" >www.yahoo.es</a>';
+        $texto_filtrado = DesymfonyTwigExtension::auto_link_text($texto);
+
+        $this->assertEquals( $texto_esperado, $texto_filtrado);
+    }
+
+    public function testURLenParrafoEsConvertida()
+    {
+        $texto          = <<<HTML
+        <p>Lorem ipsum dolor et sit www.google.es con amet</p>
+HTML;
+
+        $texto_esperado = <<<HTML
+        <p>Lorem ipsum dolor et sit <a href="http://www.google.es" target="_blank" >www.google.es</a> con amet</p>
+HTML;
+
+        $texto_filtrado = DesymfonyTwigExtension::auto_link_text($texto);
+
+        $this->assertEquals( $texto_esperado, $texto_filtrado);
+
+    }
+
+}
