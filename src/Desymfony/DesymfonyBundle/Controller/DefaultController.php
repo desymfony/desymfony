b723b3ac58b812b329911cbf20227e94ed1031b0
Javier Eguíluz <javier.eguiluz@gmail.com>
2011-06-28 23:12:20 +0200

    Punto de inicio del tutorial #1

--- src/Desymfony/DesymfonyBundle/Controller/DefaultController.php
+++ src/Desymfony/DesymfonyBundle/Controller/DefaultController.php
@@ -3,30 +3,8 @@
 namespace Desymfony\DesymfonyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
-//Aquí habrá código hecho por Javi E.
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        $em = $this->get('doctrine')->getEntityManager();
-        $ponenciasDiaUno = $em->getRepository('DesymfonyBundle:Ponencia')->findTodasDeFecha('2011-07-01');
-        $ponenciasDiaDos = $em->getRepository('DesymfonyBundle:Ponencia')->findTodasDeFecha('2011-07-02');
-
-        return $this->render('DesymfonyBundle:Default:index.html.twig', array(
-            'ponenciasDiaUno' => $ponenciasDiaUno,
-            'ponenciasDiaDos' => $ponenciasDiaDos,
-        ));
-    }
-
-    /**
-     * Muestra el contenido de una página estática (contacto, privacidad, etc.)
-     *
-     * @param string $pagina Nombre de la página que debe mostrarse
-     */
-    public function estaticaAction($pagina)
-    {
-        return $this->render(sprintf('DesymfonyBundle:Estaticas:%s.html.twig', $pagina));
-    }
-
 }
