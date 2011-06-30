7382f923732c78386060165ca923c98051f27d15
Albert Jessurum <ajessu@gmail.com>
2011-06-30 07:07:16 +0200

    Principio de tutorial6

--- src/Desymfony/DesymfonyBundle/Tests/Controller/PonenciaControllerTest.php
+++ src/Desymfony/DesymfonyBundle/Tests/Controller/PonenciaControllerTest.php
@@ -7,31 +7,8 @@ class PonenciaControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = $this->createClient();
-        $crawler = $client->request('GET', '/ponencias');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Status 200 en ponencias");
-        $this->assertTrue($crawler->filter('html:contains("Ponencias")')->count()> 0, "La vista contiene la palabra Ponencias");
+        $this->markTestIncomplete('Implementar en las Jornadas');
     }
 
-    public function testPonencia()
-    {
-        $client = $this->createClient();
-        $crawler = $client->request('GET', '/ponencia/el-modelo-doctrine2');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Status 200 en vista ponencia");
-
-        $crawler = $client->request('GET', '/ponencia/noexisto');
-        $this->assertEquals(404, $client->getResponse()->getStatusCode(), "Status 404 para /ponencia y slug inválida");
-
-    }
-
-    public function testApuntarse()
-    {
-        $client = $this->createClient();
-        $crawler = $client->request('GET', '/me-apunto-a-el-modelo-doctrine2');
-        $this->assertEquals(302, $client->getResponse()->getStatusCode(), 'El usuario no registrado es redirigido al login');
-        $crawler = $client->followRedirect();
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
-    }
 }
 
--- src/Desymfony/DesymfonyBundle/Tests/Validator/DNIValidatorTest.php
+++ src/Desymfony/DesymfonyBundle/Tests/Validator/DNIValidatorTest.php
@@ -8,20 +8,6 @@ class DNIValidatorTest extends \PHPUnit_Framework_TestCase
 {
     public function testIsValid()
     {
-        $dniValidator = new DniValidator();
-        $this->assertTrue($dniValidator->isValid("11111111H", new DNI()), "DNI válido valida");
-        $this->assertTrue($dniValidator->isValid("11111111h", new DNI()), "La letra del DNI puede ser minúscula");
-        $this->assertTrue($dniValidator->isValid("11111W", new DNI()), "DNI corto valida");
-    }
-
-    public function testIsNotValid()
-    {
-        $this->assertFalse($dniValidator->isValid("11111111A", new DNI()), "DNI con letra incorrecta no valida");
-        $this->assertFalse($dniValidator->isValid("11111111-H", new DNI()), "DNI con guion no valida");
-        $this->assertFalse($dniValidator->isValid("H11111111", new DNI()), "DNI con letra al principio no valida");
-        $this->assertFalse($dniValidator->isValid("11111111 H", new DNI()), "DNI con espacio no valida");
-        $this->assertFalse($dniValidator->isValid("11111111", new DNI()), "DNI sin letra no valida");
-        $this->assertFalse($dniValidator->isValid("11111111111111111111H", new DNI()), "DNI demasiado largo no valida");
-
+        $this->markTestIncomplete('Implementar en las Jornadas');
     }
 }
