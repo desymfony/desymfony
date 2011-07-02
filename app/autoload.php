0bfda1c7cf5e81d86e10c07f53a4ddd0701073a6
Moisés Maciá <moises.macia@ideup.com>
2011-07-02 22:28:50 +0200

    upddated IdeupSimplePaginator dependencies

--- app/autoload.php
+++ app/autoload.php
@@ -6,18 +6,20 @@ umask(0);
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Sensio'           => __DIR__.'/../vendor/bundles',
-    'JMS'              => __DIR__.'/../vendor/bundles',
+    'Symfony'                        => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
+    'Sensio'                         => __DIR__.'/../vendor/bundles',
+    'JMS'                            => __DIR__.'/../vendor/bundles',
     'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-fixtures/lib',
-    'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
-    'Monolog'          => __DIR__.'/../vendor/monolog/src',
-    'Assetic'          => __DIR__.'/../vendor/assetic/src',
-    'Metadata'         => __DIR__.'/../vendor/metadata/src',
+    'Doctrine\\Common'               => __DIR__.'/../vendor/doctrine-common/lib',
+    'Doctrine\\DBAL'                 => __DIR__.'/../vendor/doctrine-dbal/lib',
+    'Doctrine'                       => __DIR__.'/../vendor/doctrine/lib',
+    'DoctrineExtensions'             => __DIR__.'/../vendor/doctrine-extensions/lib',
+    'Monolog'                        => __DIR__.'/../vendor/monolog/src',
+    'Assetic'                        => __DIR__.'/../vendor/assetic/src',
+    'Metadata'                       => __DIR__.'/../vendor/metadata/src',
+    'Ideup'                          => __DIR__.'/../vendor/bundles',
 
-    'Desymfony'        => __DIR__.'/../src',
+    'Desymfony'                      => __DIR__.'/../src',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
